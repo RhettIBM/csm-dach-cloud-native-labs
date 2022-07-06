57f50a209b7cd00a430578a92e73f386c240a8ea
RhettIBM <102160136+RhettIBM@users.noreply.github.com>
2022-07-06 14:00:47 +0200

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Workshop!');
+  res.send('Hello Workshop - This time from console!');
 });
 
 app.listen(8080, function () {
